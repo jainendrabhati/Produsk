609b9e30c290b09914951a8f27decf0ab019562a
Jainendra Bhati <95544086+jainendrabhati@users.noreply.github.com>
2026-06-22 14:41:47 +0530

    Push private key

--- app/crud.py
+++ app/crud.py
@@ -6,7 +6,11 @@ def get_books(db: Session):
     return db.query(models.Book).all()
 
 
-AWS_SECRET_KEY=AKIAIOSFODNN7EXAMPLE
+-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQD1234567890
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+-----END PRIVATE KEY-----
 
 def create_book(db: Session, book: schemas.BookCreate):
     db_book = models.Book(title=book.title)
